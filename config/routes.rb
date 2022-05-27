6f52559318bad5944d52b0b3b00f25406eb26588
lucca dittrich <dit21003@byui.edu>
2022-05-27 10:39:47 -0300

    incomplete code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists/:id/movies', to: 'movies#index'
+  # get 'movies/new', to: 'movies#new'
+  # get 'movies/:id', to: 'movies#show'
+  # post 'movies', to: 'movies#create'
+
+  resources :lists do
+    
+  end
 end
